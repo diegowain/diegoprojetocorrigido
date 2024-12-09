2c260e1fb19158f2caf812e5a0daccee4fcd8cc8
diegowain <diegocwain@gmail.com>
2024-12-09 18:42:25 -0300

    first commit

---
+++ diego-projeto-corrigido/atividadefspidw-backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+/* rotas */
+const eventoRoutes = require('./routers/EventosRoutes')
+const despesaRoutes = require('./routers/DespesasRoutes')
+const doadorRoutes = require ('./routers/DoadoresRoutes')
+const doacaoRoutes = require ('./routers/DoacoesRoutes')
+
+const app = express( )
+const port = 3006
+app.use(express.json( ))
+app.use(cors( ))
+app.use(eventoRoutes)
+app.use(despesaRoutes)
+app.use(doadorRoutes)
+app.use(doacaoRoutes)
+app.listen(port, ( ) => `Executando na porta ${port}`)
